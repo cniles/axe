d5dd88cafa80adbc7b71e8c7549984e0d3038aa9
Craig Niles <craig.niles@cloudreach.com>
2020-08-11 21:31:44 -0400

    Add AWS Lambda Invoke API function.
    Also update docs for ListFunctions

--- aws-lambda.el
+++ aws-lambda.el
@@ -5,6 +5,10 @@
 (require 'aws-buffer-mode)
 
 (cl-defun aws-lambda--list-functions (success &key function-version next-token master-region max-items)
+  "List functions in the account and call SUCCESS when complete.
+
+FUNCTION-VERSION, MASTER-REGION and MAX-ITEMS all correspond to
+AWS API request parameters. NEXT-TOKEN is the MARKER parameter."
   (aws-api-request
    (aws-api-endpoint 'lambda aws-region)
    'lambda
@@ -15,6 +19,26 @@
 					 ("Marker" . ,next-token)
 					 ("MasterRegion" . ,master-region)
 					 ("MaxItems" . ,max-items)))))
+
+(defun aws-lambda--invoke (success function-name payload &key client-context invocation-type log-type qualifier)
+  "Invoke AWS Lambda function with name FUNCTION-NAME and PAYLOAD.
+
+SUCCESS is invoked upon completion.  Parameters CLIENT-CONTEXT,
+INVOCATION-TYPE, LOG-TYPE and QUALIFIER operate as described in
+AWS API request parameters.
+
+See `https://docs.aws.amazon.com/lambda/latest/dg/API_Invoke.html'."
+  (aws-api-request
+   (aws-api-endpoint 'lambda aws-region)
+   'lambda
+   success
+   "POST"
+   :path-segments `("2015-03-31" "functions" ,function-name "invocations")
+   :query-params (rassq-delete-all nil `(("Qualifier" . , qualifier)))
+   :headers (rassq-delete-all nil `(("X-Amz-Invocation-Type" . ,invocation-type)
+				    ("X-Amz-Log-Type" . ,log-type)
+				    ("X-Amz-Client-Context" . ,client-context)))
+   :request-payload payload))
 
 ;;;###autoload
 (cl-defun aws-lambda-list-functions (&key function-version master-region max-items)
