598be5ca238ba04e25ceebf38889225ed14b21c6
Craig Niles <niles.c@gmail.com>
2020-10-04 16:28:33 -0400

    MELPA Apply feedback for axe.el.

--- axe.el
+++ axe.el
@@ -21,7 +21,7 @@
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with axe.  If not, see <http://www.gnu.org/licenses/>.
+;; along with axe.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
