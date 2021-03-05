b9974d6402eb753b7e0088b311d9427b607d92a6
Craig Niles <craig.niles@cloudreach.com>
2021-03-05 18:17:14 -0500

    Add function to list users in a buffer.

--- axe-iam.el
+++ axe-iam.el
@@ -24,27 +24,53 @@
 ;;; Commentary:
 
 ;; axe-iam.el provides functions for interacting with AWS S3 APIs.  For
-;; example, listing buckets and their contents.
+;; example, listing users and roles.
 
 ;;; Code:
 
 (require 'axe-api)
+(require 'axe-buffer-mode)
 
-(cl-defun axe-iam--list-users (success &key marker max-items path-prefix)
+(cl-defun axe-iam--list-users (success &key next-token max-items path-prefix)
   "Call SUCCESS with the results of the IAM ListUsers API.
-MARKER specifies the start of the next page of results and should
-be set to a value provided in a previous ListUsers response.
-MAX-ITEMS is used when paginating results.  PATH-PREFIX is used
-to filter results.  See:
+NEXT-TOKEN specifies the start of the next page of
+results (Marker) and should be set to a value provided in a
+previous ListUsers response.  MAX-ITEMS is used when paginating
+results.  PATH-PREFIX is used to filter results.  See:
 `https://docs.aws.amazon.com/IAM/latest/APIReference/API_ListUsers.html'"
   (axe-api-request
    "iam.amazonaws.com"
    'iam success
    "GET"
    :parser #'axe-util--xml-read
-   :query-params (rassq-delete-all nil `(("PathPrefix" . ,path-prefix)
-					 ("Action" . "ListUsers")
-					 ("Version" . "2010-05-08")))))
+   :query-params (rassq-delete-all nil `(("Action" . "ListUsers")
+					 ("Version" . "2010-05-08")
+					 ("MaxItems" . ,max-items)
+					 ("Marker" . ,next-token)))))
+
+;;;###autoload
+(defun axe-iam-list-users ()
+  "List IAM users in a buffer."
+  (interactive)
+  (axe-list-api-results
+   #'axe-iam--list-users
+   "*axe-iam-users-list*"
+   (cl-function
+    (lambda (&key data &allow-other-keys)
+      (mapcar
+       (lambda (node)
+	 (let ((user (axe-util--xml-node-to-alist node)))
+	   (list nil (vector
+		      (format "%-30s" (alist-get 'CreateDate user))
+		      (alist-get 'UserName user)))))
+       (axe-util--search-xml-children 'member data))))
+   [("Creation Date" 30 t) ("User Name" 1 t)]
+   ()
+   ()
+   (cl-function
+    (lambda (&key data &allow-other-keys)
+      (let ((result (axe-util--xml-node-to-alist (first (axe-util--search-xml-children 'ListUsersResult data)))))
+	(alist-get 'Marker result))))))
 
 (provide 'axe-iam)
 ;;; axe-iam.el ends here
